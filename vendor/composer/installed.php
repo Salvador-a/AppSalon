8d1d65a461db0a5391db290b77c8c229a451f791
Salvador-a <salvador.sar614@outlook.com>
2023-12-16 15:01:07 -0600

    Se implemento PHP modernos, uqe seria instalar y configurar Comporse para el aulog

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'administrador/app-salon',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'ffb17560c0b45868c393d9cafd06b6a66a43137b',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'administrador/app-salon' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'ffb17560c0b45868c393d9cafd06b6a66a43137b',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
