a4f8bb37d7ae1c569da7b1b5989a792310cb9779
Salvador-a <salvador.sar614@outlook.com>
2023-12-17 17:11:14 -0600

    Se les dio estilos al formualrio pero solo a los campos de input

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,3 +1,33 @@
 <h1 class="nombre-paguina">Login</h1>
 
 <p class="descripcion-paguina">Inicia sesión con tus datos</p>
+
+<form action="/" method="POST">
+
+    <div class="campo">
+        <label for="email">Email</label>
+        <input 
+            type="email"
+            id="email"
+            placeholder="Tu Email"  
+            name="email"
+        />         
+    </div>
+
+    <div class="campo">
+        <label for="password">Passeord</label>
+        <input 
+            type="password"
+            id="password"
+            placeholder="Tu Password"
+            name="password"
+        />
+    </div>
+
+    <input type="submit" class="boton" value="Iniciar Sesión">
+</form>
+<div class="acciones">
+    <a href="/crear-cuenta">¿Aún no tienes una cuenta? Crear una </a>
+    <a href="/olvide">¿Olvidades tu password?</a>
+
+</div>
