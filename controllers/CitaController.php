0c5139fbe6c9e00a4a217d339d85c4ea7681da7e
Salvador-a <salvador.sar614@outlook.com>
2023-12-20 13:15:39 -0600

    Se creo la paguina de Citas

---
+++ controllers/CitaController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Controllers;
+
+use MVC\Router;
+
+class CitaController {
+    
+    public static function index( Router $router ) {
+
+        $router->render('cita/index', [
+
+        ]);
+
+    }
+}
+
---
+++ views/cita/index.php
@@ -0,0 +1,2 @@
+<h1 class="nombre-paguina">Crear Nueva Cita</h1>
+<p class="descripcion-paguina">Eligue tus servicios y coloca tus datos</p>
