afeba16f53769ad2ede5caeac726106062aa3566
Salvador-a <salvador.sar614@outlook.com>
2023-12-20 13:22:17 -0600

    Se implente las area de la paguina de citas'

--- views/cita/index.php
+++ views/cita/index.php
@@ -1,2 +1,21 @@
 <h1 class="nombre-paguina">Crear Nueva Cita</h1>
 <p class="descripcion-paguina">Eligue tus servicios y coloca tus datos</p>
+
+<div class="app">
+    <div id="paso-1" class="seccion">
+        <h2>Aervicios</h2>
+        <p>Eligue tus servicios a continuacion</p>
+        <div id="servicios" class="listado-servicios"></div>
+
+    </div>
+    <div id="paso-2" class="seccion">
+        <h2>Tus Datos y Cita</h2>
+        <p>Colo tus datos y fecha de tu cita</p>
+
+    </div>
+    <div id="paso-3" class="seccion">
+        <h2>Resumen</h2>
+        <p>Verifica que tu informacion sea correcta</p>
+
+    </div>
+</div>
